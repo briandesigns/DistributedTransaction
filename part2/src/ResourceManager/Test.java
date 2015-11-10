da390d17e161130430e95dc32073fb9004a0e443
brian zee <brian.zhou@mail.mcgill.ca>
2015-11-10 15:33:07 -0500

    out added

---
+++ part2/src/ResourceManager/Test.java
@@ -0,0 +1,12 @@
+package ResourceManager;
+
+import TransactionManager.TransactionManager;
+
+/**
+ * Created by brian on 06/11/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
